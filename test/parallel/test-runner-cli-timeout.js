9a3e348c6c8fd8f2505f7bed7093c807d48bf6c9
Jake Y Li <jake.yuesong@gmail.com>
2024-07-10 12:46:14 +0930

    test: remove unnecessary async

--- test/parallel/test-runner-cli-timeout.js
+++ test/parallel/test-runner-cli-timeout.js
@@ -7,13 +7,13 @@ const { test } = require('node:test');
 const cwd = fixtures.path('test-runner', 'default-behavior');
 const env = { ...process.env, 'NODE_DEBUG': 'test_runner' };
 
-test('default timeout -- Infinity', async () => {
+test('default timeout -- Infinity', () => {
   const args = ['--test'];
   const cp = spawnSync(process.execPath, args, { cwd, env });
   assert.match(cp.stderr.toString(), /timeout: Infinity,/);
 });
 
-test('timeout of 10ms', async () => {
+test('timeout of 10ms', () => {
   const args = ['--test', '--test-timeout', 10];
   const cp = spawnSync(process.execPath, args, { cwd, env });
   assert.match(cp.stderr.toString(), /timeout: 10,/);
